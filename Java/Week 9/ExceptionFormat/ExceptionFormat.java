1c15cfcd2a06298af3832209c3cf8510ea558f04
SasankaGeetanath <surya5896.gorthi@gmail.com>
2020-03-24 15:10:52 +0530

    Third Commit

---
+++ Java/Week 9/ExceptionFormat/ExceptionFormat.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ExceptionFormat {
+    public static void main( String[] args ) throws NumberFormatException, ArithmeticException {
+        Scanner scan = new Scanner(System.in);
+    
+        System.out.print("ENTER THE DIVIDENT : ");
+        Double N = scan.nextDouble();
+        System.out.print("ENTER THE DIVISOR : ");
+        Double M = scan.nextDouble();
+        scan.close();
+
+        if (N - N.intValue() != 0.0 || M - M.intValue() != 0.0)
+            throw new NumberFormatException("The data types of the numbers aren't integers.");
+        else if ( M == 0 )
+            throw new ArithmeticException("CANT DIVIDE WITH ZERO.");
+        else 
+            System.out.println( "THE VALUE IS " + N/M );
+    }
+}
